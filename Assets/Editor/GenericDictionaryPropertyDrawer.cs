51e2d073ebac825414c640cc774b95952ac1cf19
eriksson.e.erik@gmail.com <eriksson.e.erik@gmail.com>
2021-01-15 22:15:46 +0100

    Add initial support for Reorderable Dictionaries and Unity 2020.2.
    See issue #6 for more info of known issues and why this has taken longer than expected.

--- Assets/Editor/GenericDictionaryPropertyDrawer.cs
+++ Assets/Editor/GenericDictionaryPropertyDrawer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 /// <summary>
@@ -11,125 +12,40 @@ public class GenericDictionaryPropertyDrawer : PropertyDrawer
 {
     static float lineHeight = EditorGUIUtility.singleLineHeight;
     static float vertSpace = EditorGUIUtility.standardVerticalSpacing;
-    static float combinedPadding = lineHeight + vertSpace;
-    int valuePropCount;
 
     public override void OnGUI(Rect pos, SerializedProperty property, GUIContent label)
     {
-        // Setup variables used for drawing.
+        // Draw list.
+        var list = property.FindPropertyRelative("list");
+        string fieldName = ObjectNames.NicifyVariableName(fieldInfo.Name);
         var currentPos = new Rect(lineHeight, pos.y, pos.width, lineHeight);
-        bool isExpanded = false;
-        var propCopy = property.Copy();
-        var enumerator = property.GetEnumerator();
+        EditorGUI.PropertyField(currentPos, list, new GUIContent(fieldName), true);
 
-        // Iterate properties and draw them.
-        while (enumerator.MoveNext())
-        {
-            var currentProp = ((SerializedProperty)enumerator.Current);
-            if (currentProp.name == "list")
-            {
-                // Draw list header and expand arrow.
-                string fieldName = ObjectNames.NicifyVariableName(fieldInfo.Name);
-                EditorGUI.PropertyField(currentPos, currentProp, new GUIContent(fieldName));
-                isExpanded = currentProp.isExpanded;
-            }
-            else if (currentProp.name == "size" && isExpanded)
-            {
-                // Draw size property.
-                EditorGUI.indentLevel++;
-                currentPos = new Rect(currentPos.x, currentPos.y + lineHeight, pos.width, lineHeight);
-                EditorGUI.PropertyField(currentPos, currentProp, new GUIContent("Size"));
-                currentPos.y += vertSpace;
-            }
-            else if (isExpanded && (currentProp.name == "Key" || currentProp.name == "Value"))
-            {
-                // Setup position and draw KeyValue-properties.
-                var entryPos = new Rect(currentPos.x, currentPos.y + combinedPadding, pos.width, lineHeight);
-                if (currentProp.isExpanded)
-                {
-                    currentPos.y += valuePropCount * combinedPadding;
-                }
-                EditorGUI.PropertyField(entryPos, currentProp, new GUIContent(currentProp.name), currentProp.isExpanded);
-
-                // Add padding.
-                if (currentProp.name == "Value")
-                {
-                    currentPos.y += combinedPadding + vertSpace;
-                }
-                else
-                {
-                    currentPos.y += lineHeight + vertSpace;
-                }
-            }
-        }
-
-        // Draw key collision warning box.
-        bool keyCollision = propCopy.FindPropertyRelative("keyCollision").boolValue;
+        // Draw key collision warning.
+        var keyCollision = property.FindPropertyRelative("keyCollision").boolValue;
         if (keyCollision)
         {
-            var entryPos = new Rect(lineHeight, currentPos.y + combinedPadding, pos.width, lineHeight * 2f);
+            currentPos.y += EditorGUI.GetPropertyHeight(list, true) + vertSpace;
+            var entryPos = new Rect(lineHeight, currentPos.y, pos.width, lineHeight * 2f);
             EditorGUI.HelpBox(entryPos, "Duplicate keys will not be serialized.", MessageType.Warning);
         }
     }
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        // Make copies of property so we can iterate it more than once
-        var listCopy = property.Copy();
-        var valueCopy = property.Copy();
-
-        // Count number of data-properties and expanded value-properties.
-        int listCount = 0;
-        int expandedCount = 0;
-        var enumerator = property.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            var currentProp = ((SerializedProperty)enumerator.Current);
-            if (currentProp.name == "data")
-            {
-                listCount++;
-            }
-            else if (currentProp.isExpanded && currentProp.name == "Value")
-            {
-                expandedCount++;
-            }
-        }
-
-        // Count number of sub-properties inside Value-property.
-        enumerator = valueCopy.GetEnumerator();
-        while (enumerator.MoveNext())
-        {
-            var currentProp = ((SerializedProperty)enumerator.Current);
-            if (currentProp.name == "Value" && currentProp.isExpanded)
-            {
-                valuePropCount = currentProp.CountInProperty() - 1;
-                break;
-            }
-        }
-
-        // Accumulate height for all properties.
         float totHeight = 0f;
 
+        // Height of KeyValue list.
+        var listProp = property.FindPropertyRelative("list");
+        totHeight += EditorGUI.GetPropertyHeight(listProp, true);
+
         // Height of key collision warning.
-        bool keyCollision = listCopy.FindPropertyRelative("keyCollision").boolValue;
+        bool keyCollision = property.FindPropertyRelative("keyCollision").boolValue;
         if (keyCollision)
         {
             totHeight += lineHeight * 2f + vertSpace;
         }
 
-        // Height of KeyValue list.
-        var listProp = listCopy.FindPropertyRelative("list");
-        if (listProp.isExpanded)
-        {
-            var t = EditorGUI.GetPropertyHeight(listProp, false);
-            totHeight += lineHeight * 2f + vertSpace;  // list header and size fields
-            totHeight += listCount * 2f * combinedPadding + listCount * vertSpace;  // list contents fields
-            totHeight += expandedCount * valuePropCount * combinedPadding;  // expanded value fields
-            return totHeight;
-        }
-        else
-        {
-            return totHeight + lineHeight;
-        }
+        return totHeight;
     }
 }
